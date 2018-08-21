3d8e530c6d872edec362624a986fa8037538005b
rishav <rishavchauhan1396@gmail.com>
2018-08-22 05:05:57 +0530

    Hello program

---
+++ hello.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+public class hello{
+	public static void main(String[] a){
+		System.out.print("Hello World!");
+	}
+}
+-----------------------
+2.
+public class hello{
+	public static void main(String[] a){
+		System.out.print("Rishav Chauhan");
+	}
+}
