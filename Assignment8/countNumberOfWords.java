8a50d83d9ff9d740e4f82964a30106c1a51b3d65
rishav1397 <42260185+rishav1397@users.noreply.github.com>
2018-09-07 18:28:08 +0530

    String

---
+++ Assignment8/countNumberOfWords.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class countNumberOfWords{
+	public static void main(String[] a1){
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		int c=0;
+		for(str[i]=0;str[i]!='\0';i++)
+			if(str[i]==' ')
+				c++;
+			System.out.println(c+1);
+	}
+}
